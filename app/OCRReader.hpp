e0fef4969cd15d491c2e5a01378bd2fd2434d96d
Bharatvaj <bharatvajph@gmail.com>
2018-04-22 20:38:24 +0530

    Temp 7

--- app/OCRReader.hpp
+++ app/OCRReader.hpp
@@ -1,4 +1,13 @@
 #ifndef _OCR_READ_H
 #define _OCR_READ_H "OCRReader"
-
+#include <opencv2/text/ocr.h>
+using namespace cv::text;
+class OCRReader
+{
+  public:
+    OCRReader()
+    {
+    }
+    
+};
 #endif
